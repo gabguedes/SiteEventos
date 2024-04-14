74faf547b8ff3f2490f5fef4d2c664fda340370c
Gabriel Guedes <gabriel4g.crispim@gmail.com>
2024-04-14 16:42:40 -0300

    Date format e segunda versao frontend

--- src/main/java/br/com/fiap/evento/model/Evento.java
+++ src/main/java/br/com/fiap/evento/model/Evento.java
@@ -1,9 +1,7 @@
 package br.com.fiap.evento.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -24,10 +22,11 @@ public class Evento {
     @Size(min = 3, message = "O nome deve ter pelo menos 3 caracteres.")
     private String nome;
 
-    @NotEmpty
-    private String data;
-//    @DateTimeFormat(pattern = "yyyy-MM-dd")
-//    private Date data;
+    @NotNull(message = "Data requerida.")
+    @Future(message = "A data deve ser em um dia futuro.")
+//    private String data;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date data;
 
     @NotBlank(message = "Campo Requerido.")
     private String url;
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,6 +2,6 @@ INSERT INTO tb_cidade(nome, estado) VALUES('São Paulo', 'São Paulo');
 INSERT INTO tb_cidade(nome, estado) VALUES('Rio de Janeiro', 'Rio de Janeiro');
 INSERT INTO tb_cidade(nome, estado) VALUES('Belo Horizonte', 'Minas Gerais');
 
-INSERT INTO tb_evento(nome, data, url, cidade_id) VALUES('Lollapalooza', '2024-03-23', 'https://www.lollapaloozabr.com/' , 1);
+INSERT INTO tb_evento(nome, data, url, cidade_id) VALUES('Lollapalooza', '2025-03-23', 'https://www.lollapaloozabr.com/' , 1);
 INSERT INTO tb_evento(nome, data, url, cidade_id) VALUES('Rock In Rio', '2024-09-22', 'https://rockinrio.com/',2);
 INSERT INTO tb_evento(nome, data, url, cidade_id) VALUES('Zeca Pagodinho', '2024-11-23', 'https://www.ticket360.com.br/',3);
